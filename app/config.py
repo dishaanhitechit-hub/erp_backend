009257c1221a30f07e74a9dff4414dddfebf542c
rohanmahi123 <rohan.soumyajit@gmail.com>
2026-04-20 15:59:20 +0530

    Edited permission get_all_user

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config:
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
     DB_PORT = os.getenv("DB_PORT")
-
+    JWT_ACCESS_TOKEN_EXPIRES = False
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://postgres:RdhKCuXIPXHDwIMGfTfSfOLPyWjRazSc@hopper.proxy.rlwy.net:18844/railway"
     )
