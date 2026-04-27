f59ce312ef866dcc35b2e5daf7fc723e42afd008
rohanmahi123 <rohan.soumyajit@gmail.com>
2026-04-27 14:10:38 +0530

    Edited permission get_all_user

--- app/cloudinary_config.py
+++ app/cloudinary_config.py
@@ -1,10 +1,20 @@
 # app/cloudinary_config.py
 
+# import cloudinary
+#
+# cloudinary.config(
+#     cloud_name="dn3cq0lbu",
+#     api_key="811429497525459",
+#     api_secret="IoFNga0m03AXpBxbCSclBgtjEP8",
+#     secure=True
+# )
+
+import os
 import cloudinary
 
 cloudinary.config(
-    cloud_name="dn3cq0lbu",
-    api_key="811429497525459",
-    api_secret="IoFNga0m03AXpBxbCSclBgtjEP8",
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
 )
