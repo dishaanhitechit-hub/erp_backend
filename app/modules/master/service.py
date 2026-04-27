db601bf1ba4d7605a007ad8adc11fe8fdcb46237
rohanmahi123 <rohan.soumyajit@gmail.com>
2026-04-27 14:07:12 +0530

    Edited permission get_all_user

--- app/modules/master/service.py
+++ app/modules/master/service.py
@@ -199,10 +199,10 @@ def get_vendor_by_id(ledgerId):
         "registeredAddress": vendor.registered_address,
         "corporateAddress": vendor.corporate_address,
 
-        # "vendorCategory": (
-        #     vendor.vendor_category.category_name
-        #     if vendor.vendor_category else None
-        # ),
+        "categoryName": (
+            vendor.vendor_category.category_name
+            if vendor.vendor_category else None
+        ),
         "categoryId": vendor.category_id,
         "pan": vendor.pan,
         "gstin": vendor.gstin,
