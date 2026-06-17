10d4c9fd9d75b3c0a85da8c4fb08162035a5a371
rohanmahi123 <rohan.soumyajit@gmail.com>
2026-06-17 12:30:05 +0530

    dbms

--- app/config.py
+++ app/config.py
@@ -4,11 +4,11 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY") or "super-secret"
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY") or "jwt-super-secret"
 
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_NAME = os.getenv("DB_NAME")
-    DB_PORT = os.getenv("DB_PORT")
+    # DB_USER = os.getenv("DB_USER")
+    # DB_PASSWORD = os.getenv("DB_PASSWORD")
+    # DB_HOST = os.getenv("DB_HOST")
+    # DB_NAME = os.getenv("DB_NAME")
+    # DB_PORT = os.getenv("DB_PORT")
     JWT_ACCESS_TOKEN_EXPIRES = False
     # SQLALCHEMY_DATABASE_URI = (
     #     f"postgresql://postgres:JoFJPHzrFFzKosJdRnneXRYLtUxjiTWe@yamabiko.proxy.rlwy.net:57509/railway"
