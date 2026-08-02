dce3c4b685d29f58c118e814c10af99bf88bcf82
rohanmahi123 <rohan.soumyajit@gmail.com>
2026-08-02 18:25:53 +0530

    biiling

--- app/modules/project_mgmt/custom_billing/billing/service.py
+++ app/modules/project_mgmt/custom_billing/billing/service.py
@@ -174,7 +174,7 @@ def _serialize_items(items):
             "itemDescription": item.item_name_desc,
             "unit":                item.unit,
             "claimQty":            float(item.claim_qty   or 0),
-            "orderQty":item.og_sale_order_order_qty,
+            "orderQty": float(item.og_so_item.order_qty or 0) if item.og_so_item else None,
             "rate":                float(item.rate         or 0),
             "amount":              float(item.amount       or 0),
             "gstPercent":          float(item.gst_percent  or 0),
